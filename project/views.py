874a42fe685cc1feb62b2c574d16e0fd360ec1bb
Scofield1 <adebayoabdulrauf1@gmail.com>
2022-10-25 19:07:15 +0100

    changes in html

--- project/views.py
+++ project/views.py
@@ -45,7 +45,7 @@ def login_page(request):
 
     if request.method == 'POST':
         username = request.POST.get('username')
-        password = request.POST.get('password')
+        password = request.POST.get('password1')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
